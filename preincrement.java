1c0a577f51e5a52b49f0155a83087115e5c7b8bf
Arus Rangra <arushrangra@gmail.com>
2026-06-10 23:00:47 +0530

    Rename class Normaloperators to preincrement

---
+++ preincrement.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class  preincrement{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+
+        b = ++a; 
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+
+    }
+}
