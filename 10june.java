35262869af03bc2c16162889bf60b298a5dc06d0
Arus Rangra <arushrangra@gmail.com>
2026-06-10 20:07:59 +0530

    Add conditional statement example in 10june.java

---
+++ 10june.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class conditionalst {
+    public static void main(String args[]) {
+        int a = 8;
+        int b = 8;
+
+        a++;
+        b++;
+        if (a > 5 && a > 8 && b > 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
