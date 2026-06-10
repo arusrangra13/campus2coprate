d13c63bc60f4a1e5e39bfd74c4b4425d9324865c
Arus Rangra <arushrangra@gmail.com>
2026-06-10 22:35:06 +0530

    Rename class from Normaloperators to Airthmaticoperator

---
+++ Airthmaticoperator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Airthmaticoperator{
+    public static void main(String[] args) {
+        int a = 89;
+        int b = 45;
+        System.out.println("a+b=" + (a + b));
+        System.out.println("a-b=" + (a - b));
+        System.out.println("a/b=" + (a / b));
+        System.out.println("a*b=" + (a * b));
+        System.out.println("a%b=" + (a % b));
+    }
+
+}
