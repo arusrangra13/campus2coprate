9fe5bf1a105e42c3ca41ac381786d5bad36baa78
Arus Rangra <arushrangra@gmail.com>
2026-06-11 11:05:38 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,13 @@
+public class loops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("hello");
+        }
+        int j = 5;
+        while (j <= 9) {
+            System.out.println("hlo hlo");
+        j++;
+        }
+
+    }
+}
