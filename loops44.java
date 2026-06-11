e2a51e0118f0de4cbd8cff9810e7cd91afa10e5f
Arus Rangra <arushrangra@gmail.com>
2026-06-11 11:20:13 +0530

    Add conditional loop to skip printing 99

---
+++ loops44.java
@@ -0,0 +1,12 @@
+public class conditionalst {
+    public static void main(String[] args) {
+        // int i = 10;
+        for (int i = 0; i < 101; i++) {
+
+            if (i == 99) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
