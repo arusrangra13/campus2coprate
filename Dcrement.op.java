60a481ce95068f973dcc385f0bfc550691e024a9
Arus Rangra <arushrangra@gmail.com>
2026-06-15 22:28:20 +0530

    Update Dcrement.op.java

--- Dcrement.op.java
+++ Dcrement.op.java
@@ -1,1 +1,26 @@
+// post decrement
+import java.util.*;
 
+public class decrementop {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 0;
+        b = a--;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+    
+}
+// pre decrement
+import java.util.*;
+
+public class decrementop {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 0;
+        b = --a;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+    
+}
