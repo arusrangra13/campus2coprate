ee07bcaf3272f3202b97b2abe0be2a44ea2ec309
Arus Rangra <arushrangra@gmail.com>
2026-06-16 22:56:40 +0530

    Create 16juneArray.java

---
+++ 16juneArray.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Arraybasic{
+    public static void main(String[] args){
+        int arr[]= {10,20,30,40};
+        int b[]=arr;
+        System.out.println(b[3]);
+    }
+}
