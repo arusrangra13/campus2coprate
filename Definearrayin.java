a7292bc8851ca2ca63788406a24c1f95615575f0
Arus Rangra <arushrangra@gmail.com>
2026-06-16 23:11:55 +0530

    Create Definearrayin.java

---
+++ Definearrayin.java
@@ -0,0 +1,17 @@
+// Array 
+import java.util.*;
+public class Arraybasic{
+    public static void main(String[] args){
+        int [] marks = new int[5];
+        marks[0]=67; //mpmc
+        marks[1]=78; //python
+        marks[2]=79; //dsa
+        marks[3]=99; //human values
+        marks[4]=66; //Cn
+        for(int i =0;i<5;i++){
+            System.out.println(marks[i]);
+        }
+    
+
+    }
+}
