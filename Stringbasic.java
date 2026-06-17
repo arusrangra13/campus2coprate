7ab74e19699c6432a51295125e8420800d86718a
Arus Rangra <arushrangra@gmail.com>
2026-06-17 22:58:37 +0530

    Update Stringbasic.java

--- Stringbasic.java
+++ Stringbasic.java
@@ -25,3 +25,18 @@ public class Stringbasic{
     }
 }
 
+//-----------------------------------------------------------------------
+// To reverse in charAt
+import java.util.*;
+
+public class Stringbasic{
+      public static void main(String[] args){
+        String str = "Arus rangra";
+        System.out.println(str);
+        
+          for (int i = str.length()-1;i>=0;i--)
+                System.out.println(str.charAt(i));
+        
+        }
+}
+
