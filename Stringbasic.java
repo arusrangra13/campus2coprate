c16848aeac0178c003b0efa4f460ddcb5e7be741
Arus Rangra <arushrangra@gmail.com>
2026-06-17 21:04:07 +0530

    Update Stringbasic.java

--- Stringbasic.java
+++ Stringbasic.java
@@ -1,1 +1,14 @@
 
+import java.util.*;
+
+public class Stringbasic{
+      public static void main(String[] args){
+        String str = "Arus rangra";
+        System.out.println(str);
+        
+    }
+}
+
+// ---------------------------------------------------------------------
+
+
