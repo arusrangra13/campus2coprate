8c00e62c58f3b8d3764eba430f081664dcd5f303
Arus Rangra <arushrangra@gmail.com>
2026-06-17 21:12:38 +0530

    Add charAt example in Stringbasic.java

--- Stringbasic.java
+++ Stringbasic.java
@@ -10,5 +10,18 @@ public class Stringbasic{
 }
 
 // ---------------------------------------------------------------------
+// charAt
+import java.util.*;
 
+public class Stringbasic{
+      public static void main(String[] args){
+        String str = "Arus rangra";
+        System.out.println(str);
+
+        for (int i =0;i<str.length();i++)
+             System.out.println(str.charAt(i));
+            
+        
+    }
+}
 
