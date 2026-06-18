7e3bdcf0418924411a40e23544ae068fce2a16bc
Arus Rangra <arushrangra@gmail.com>
2026-06-18 20:32:58 +0530

    Update Stringbasic.java

--- Stringbasic.java
+++ Stringbasic.java
@@ -147,7 +147,6 @@ public class Stringbasic{
     Scanner sc = new Scanner(System.in);
 
     String str=sc.nextLine();
-    System.out.println(str);
     String str1="";
     for(int i=str.length()-1;i>=0;i--)
 
