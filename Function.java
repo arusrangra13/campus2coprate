8bce639672b16f554dbe4dffe830f48c80c952ec
Arus Rangra <arushrangra@gmail.com>
2026-06-26 23:04:14 +0530

    Update Function.java

--- Function.java
+++ Function.java
@@ -1,1 +1,17 @@
+import java.util.*;
+public class Functions {
+    public static void printName(String name) {
+        System.out.println(name);
+        return;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        printName(name);
+
+    }
+}
+
+//=================================================================
 
