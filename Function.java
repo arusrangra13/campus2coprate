dabbc1df3765aef44fa19f7f75268498efaea0ca
Arus Rangra <arushrangra@gmail.com>
2026-06-27 20:47:52 +0530

    Update Function.java

--- Function.java
+++ Function.java
@@ -48,3 +48,24 @@ public class Functions {
         nameOfStudent(name);
     }
 }
+
+//=====================================================================
+//Multiple of three
+import java.util.*;
+public class Functions {
+    public static int calculateSum(int a ,int b,int c){
+        int mul = a*b*c;
+        return mul;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int mul = calculateSum(a,b,c);
+        System.out.println("mul of three no:"+ mul);
+
+    }
+}
+//=====================================================================
