83bb03396fc8e87bbb79342e85d555c63c1bb794
Arus Rangra <arushrangra@gmail.com>
2026-06-27 17:17:53 +0530

    Implement sum calculation and student name output
    Added a method to calculate the sum of two integers and print the result.

--- Function.java
+++ Function.java
@@ -15,3 +15,36 @@ public class Functions {
 
 //=================================================================
 
+import java.util.*;
+public class Functions {
+    public static int calculateSum(int a ,int b){
+        int sum = a+b;
+        return sum;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = calculateSum(a,b);
+        System.out.println("sum of 2 numbers:"+sum);
+
+    }
+}
+
+//=====================================================================
+//practice
+import java.util.*;
+public class Functions {
+    public static void nameOfStudent(String name){
+        System.out.println(name);
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        nameOfStudent(name);
+    }
+}
