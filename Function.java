811af9422f91efea29ff58e0ed3608b0eaf3b495
Arus Rangra <arushrangra@gmail.com>
2026-06-29 22:50:26 +0530

    Update Function.java

--- Function.java
+++ Function.java
@@ -96,3 +96,49 @@ public class Functions {
         
 }
 
+//=====================================================================
+
+import java.util.*;
+public class Functions {
+    public static int calculateSum(int a ,int b,int c){
+        int mul = a*b*c;
+        return mul;
+    }
+      public static void childName(String name){
+        System.out.println(name);
+      }
+
+      public static void StudentsNames(String names){
+        System.out.println(names);
+      }
+
+      public static int Sum(int i, int j, int k){
+        int Sum = i+j+k;
+        return Sum;
+      }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // name
+        String name = sc.nextLine();
+        childName(name);
+        // mul
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int mul = calculateSum(a,b,c);
+        System.out.println("mul of three no:"+mul);
+        sc.nextLine();  
+        // names
+        String names = sc.nextLine();
+        StudentsNames("Students names:"+names);
+        // Sum
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        int k = sc.nextInt();
+        int Sum = Sum(i,j,k);
+        System.out.println("Sum of four no:"+Sum);
+
+    }
+        
+}
+
