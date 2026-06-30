854066de768117e8150f0d1d4126185c38755e84
Arus Rangra <arushrangra@gmail.com>
2026-06-30 22:09:20 +0530

    Update oops.java

--- oops.java
+++ oops.java
@@ -1,1 +1,22 @@
 // First oops code
+import java.util.*;
+
+class Pen{
+    String color;
+    String type; // Ballpen or Gel pen
+
+    public void write(){
+        System.out.println("Writng my name Arus Rangra");
+
+    }
+}
+public class Oops {
+    public static void main(String[] args){
+        Pen pen1 = new Pen();
+        pen1.color = "blue";
+        pen1.type = "gel";
+
+        pen1.write();
+    }
+    
+}
