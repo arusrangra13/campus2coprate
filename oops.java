37b481419becd420b0ddf319143b300e11f98dee
Arus Rangra <arushrangra@gmail.com>
2026-06-30 22:39:13 +0530

    Update oops.java

--- oops.java
+++ oops.java
@@ -20,3 +20,28 @@ public class Oops {
     }
     
 }
+
+// =================================================================================
+//car Example
+import java.util.*;
+
+class Car{
+    String color;
+    String type; // Sedan or Suv
+
+    public void display(){
+        System.out.println("it is a Sedan car");
+
+    }
+}
+public class Oops {
+    public static void main(String[] args){
+        Car Car1 = new Car();
+        Car1.color = "blue";
+        Car1.type = "Sedan";
+
+        Car1.display();
+    }
+    
+}
+// =================================================================================
