c792b6e54344003f800b069341114d223eed3098
Arus Rangra <arushrangra@gmail.com>
2026-07-01 20:36:45 +0530

    Add Pen class with display and print methods

--- oops.java
+++ oops.java
@@ -45,3 +45,49 @@ public class Oops {
     
 }
 // =================================================================================
+import java.util.*;
+
+class Pen{
+    String color;
+    String brand;
+    int price;
+
+    public void display(){
+        System.out.println("Hlo my name is Arus Rangra");
+    }
+    public void printColor(){
+        System.out.println(this.color);
+    }
+
+    public void printBrand(){
+        System.out.println(this.brand);
+    }
+    public int printPrice(){
+        System.out.println(this.price);
+        return this.price;
+
+    }
+}
+public class Oops {
+    public static void main(String[] args){
+
+        Pen pen1 = new Pen();
+        pen1.color = "yellow";
+        pen1.brand="ratio";
+        pen1.price=10;
+
+        Pen pen2 = new Pen();
+        pen2.color = "red";
+        pen2.brand="Natraj";
+        pen2.price=20;
+
+        pen1.display();
+        pen1.printColor();
+        pen2.printColor();
+        pen1.printBrand();
+        pen2.printBrand();
+        pen1.printPrice();
+        pen2.printPrice();
+    }
+}
+ 
