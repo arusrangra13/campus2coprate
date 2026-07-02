e8275887dd31ff455e7df9acdd01e7d478a36c1e
Arus Rangra <arushrangra@gmail.com>
2026-07-02 22:41:00 +0530

    Update Inheritence.java

--- Inheritence.java
+++ Inheritence.java
@@ -21,3 +21,37 @@ public class Oops {
        
     }
 }
+
+//=============================================================================================
+//Multi level inheritence
+import java.util.*;
+class Shape{
+    public void area(){
+        System.out.println("Display area");
+    }
+
+}
+
+class Triangle extends Shape{
+    public void area(int l , int h){
+        System.out.println(l*h/2);
+    }
+}
+
+class EquilateralTraingle extends Triangle{
+    public void  area(int l,int h){
+        System.out.println(l*h/2);
+    }
+
+}
+public class Oops {
+    public static void main(String[] args){
+        
+        Triangle t1 = new Triangle();
+        t1.area(34, 66);
+
+        EquilateralTraingle e1 = new EquilateralTraingle();
+        e1.area(34, 66);
+       
+    }
+}
