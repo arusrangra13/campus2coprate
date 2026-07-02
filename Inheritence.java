1420be69d9703f2dd76feffeffd98e3d77eb35e8
Arus Rangra <arushrangra@gmail.com>
2026-07-02 22:47:18 +0530

    Update Inheritence.java

--- Inheritence.java
+++ Inheritence.java
@@ -55,3 +55,45 @@ public class Oops {
        
     }
 }
+
+//=================================================================================
+// Hirearchical inheritance 
+import java.util.*;
+class Shape{
+    public void area(){
+        System.out.println("Display area");
+    }
+
+}
+
+class Triangle extends Shape{
+    public void area(int l , int h){
+        System.out.println(l*h/2);
+    }
+}
+
+class EquilateralTraingle extends Shape{
+    public void  area(int l,int h){
+        System.out.println(l*h/2);
+    }
+
+}
+
+class Circle extends Shape{
+    public void area(int r){
+        System.out.println(3.14*r*r);
+    }
+}
+public class Oops {
+    public static void main(String[] args){
+        
+        Triangle t1 = new Triangle();
+        t1.area(34, 66);
+
+        EquilateralTraingle e1 = new EquilateralTraingle();
+        e1.area(34, 66);
+       
+        Circle c1 = new Circle();
+        c1.area(5);
+    }
+}
