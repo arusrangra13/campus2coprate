014f423c0be142af603e464da114e19ee50c4111
Arus Rangra <arushrangra@gmail.com>
2026-07-05 22:12:02 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -14,3 +14,25 @@ public class arraylist {
 
     }
 }
+
+//============================================================================================
+// get element
+import java.util.*;
+
+public class arraylist {
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        //add elements
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+
+        //get elements
+        int element = list.get(0);
+        System.out.println(element);
+    }
+}
+ 
