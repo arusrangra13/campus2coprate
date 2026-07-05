9e39265b9a755bcf0fc8645913686c2c3392e136
Arus Rangra <arushrangra@gmail.com>
2026-07-05 22:26:18 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -122,3 +122,40 @@ public class arraylist {
     }
 }
 
+//=========================================================================================
+import java.util.*;
+
+public class arraylist {
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        //add elements
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+
+        //get elements
+        int element = list.get(0);
+        System.out.println(element);
+
+        // Add element between
+        list.add(2,33);
+        System.out.println(list);
+
+        //set element
+        list.set(0,99);
+        System.out.println(list);
+
+        //Delete/remove element
+        list.remove(2);
+        System.out.println(list);
+
+        //size
+        int size = list.size();
+        System.out.println(size);
+    }
+}
+
+//======================================================================================
