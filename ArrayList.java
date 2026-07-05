cd45a312afd280cb3f50be39624a9e9d3e56f506
Arus Rangra <arushrangra@gmail.com>
2026-07-05 22:07:12 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,1 +1,16 @@
+// Simple ArrayList
+import java.util.*;
 
+public class arraylist {
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        //add elements
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+
+    }
+}
