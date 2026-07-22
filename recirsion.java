1a33fc93cc4546337d3a7bc291bfa54f3551b9c5
Arus Rangra <arushrangra@gmail.com>
2026-07-22 22:44:54 +0530

    Update recirsion.java

--- recirsion.java
+++ recirsion.java
@@ -38,5 +38,25 @@ public class recursion{
     }
 
 }
-     
+ 
+
+//====================================================================    
+//print sum of the first n natural numbers.
+import java.util.*;
+public class recursion{
+    public static void printSum(int i,int n,int sum){
+        if(i==n){
+            sum+=i;
+            System.out.println(sum);
+            return;
+        }
+        sum+=i;
+        printSum(i+1,n,sum);
+    }
+    public static void main(String [] args){
+        printSum(1,5,0);
+
+    }
+}
+
 
